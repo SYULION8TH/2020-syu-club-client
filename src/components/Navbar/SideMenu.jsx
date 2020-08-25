dc37140b9bd3751af6499286f832ce2d788ee9e1
KyeongSoo Yoo <dev.yoogomja@gmail.com>
2020-08-25 16:53:41 +0900

    로그인 상태 완성 #10
    - 디자인 반영 1차 완료
    - 로그인 상태 관리 추가

--- src/components/Navbar/SideMenu.jsx
+++ src/components/Navbar/SideMenu.jsx
@@ -1,21 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import CircleFramedImage from '../CircleFramedImage';
 import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getUserInfo } from '../../modules/User';
+import { isNullOrUndefined } from 'util';
 
 const SideMenu = (props) => {
+    useEffect(() => {
+        if (!isNullOrUndefined(props.info.error)) {
+            if (
+                props.info.error.response.status === 401 ||
+                props.info.error.response.status === 403
+            ) {
+                // 로그인이 안된 경우
+            } else {
+                // 실제 통신에 에러가 발생한 경우
+            }
+        } else if (!isNullOrUndefined(props.info.data)) {
+            console.log(props.info.data);
+        } else {
+            console.log('requested..');
+        }
+    }, [props.info.loading]);
+
     return (
         <div className={`navbar-content-container ${props.isOpened ? 'active' : ''}`}>
             <div className="user-info-container navbar-content-wrapper">
                 <CircleFramedImage
-                    imgUrl="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/s750x750/105517799_699215570933939_4504126597064258434_n.jpg?_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_cat=111&_nc_ohc=Lo0SPzx3WowAX-fFN1x&oh=7730262a5a596937b2efe53e98219ed5&oe=5F681808"
-                    imgAlt="테스트 이미지"
+                    imgUrl={
+                        isNullOrUndefined(props.info.data)
+                            ? ''
+                            : props.info.data.user_profile.profile
+                    }
+                    imgAlt={
+                        isNullOrUndefined(props.info.data)
+                            ? '미 로그인 유저 이미지'
+                            : `${props.info.data.user.username} 프로필 이미지`
+                    }
                     width={50}
                     height={50}
                 />
                 <div className="user-info-text">
-                    <p className="user-info-name">유경수</p>
-                    <p className="user-info-email">dev.yoogomja@gmail.com</p>
+                    {!isNullOrUndefined(props.info.data) ? (
+                        <>
+                            <p className="user-info-name">{props.info.data.user.username}</p>
+                            <p className="user-info-email">{props.info.data.user.email}</p>
+                        </>
+                    ) : (
+                        <p className="user-info-name">로그인이 필요합니다</p>
+                    )}
                 </div>
             </div>
             <div className="menu-container  navbar-content-wrapper">
@@ -29,12 +64,44 @@ const SideMenu = (props) => {
                     활동 포스팅 목록
                 </Link>
             </div>
-            <div className="club-list-container  navbar-content-wrapper"></div>
+            <div className="club-list-container  navbar-content-wrapper">
+                <div className="club-list-wrapper">
+                    <p className="club-list-title">관심 동아리</p>
+                    <ul className="club-list">
+                        <li className="club-list-item">
+                            <Link className="club-list-item-link" to="">
+                                동아리 1
+                            </Link>
+                        </li>
+                        <li className="club-list-item">
+                            <Link className="club-list-item-link" to="">
+                                동아리 1
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="club-list-wrapper">
+                    <p className="club-list-title">가입 동아리</p>
+                    <ul className="club-list">
+                        <li className="club-list-item">
+                            <Link className="club-list-item-link" to="">
+                                동아리 1
+                            </Link>
+                        </li>
+                        <li className="club-list-item">
+                            <Link className="club-list-item-link" to="">
+                                동아리 1
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
 
 SideMenu.propTypes = {
+    info: PropTypes.object.isRequired,
     isOpened: PropTypes.bool.isRequired,
 };
 
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,16 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Navbar.scss';
 import AnimatedMenuIcon from './AnimatedMenuIcon';
 
 import CircleFramedImage from '../CircleFramedImage';
 import SideMenu from './SideMenu';
+import { useSelector, useDispatch } from 'react-redux';
+import { getUserInfo } from '../../modules/User';
+import { isNullOrUndefined } from 'util';
 
 export const Navbar = (props) => {
+    const { info } = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+
+    const fn = {
+        user: {
+            fetch: () => {
+                dispatch(getUserInfo());
+            },
+        },
+    };
+
+    useEffect(() => {
+        if (isNullOrUndefined(info.data)) {
+            fn.user.fetch();
+        }
+    }, []);
+
     const [menuOpened, setMenuOpened] = useState(true);
     return (
         <div className={`navbar-container`}>
             <AnimatedMenuIcon isOpened={menuOpened} setIsOpened={(v) => setMenuOpened(v)} />
-            <SideMenu isOpened={menuOpened} />
+            <SideMenu isOpened={menuOpened} info={info} />
         </div>
     );
 };
