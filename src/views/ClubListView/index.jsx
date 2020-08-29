e450b4cb009ab23acbf2b2d3b3ef1cdddc3985e9
Brave Kim <ygflove@likelion.org>
2020-08-29 17:56:33 +0900

    헤더완료 #15

--- src/views/ClubListView/index.jsx
+++ src/views/ClubListView/index.jsx
@@ -1,9 +1,93 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
+import { ClubsAPI } from '../../api';
+import { PostCard } from '../../components';
+
+import './scss/style.scss';
+
+import { BsBarChartFill, BsBatteryFull, BsWifi, BsFilterLeft } from 'react-icons/bs';
+import { AiOutlineSearch } from 'react-icons/ai';
 
 const ClubListView = () => {
+    const fetchClubList = {
+        data: [],
+        fetch: async () => {
+            const result = [
+                {
+                    id: '1',
+                    title: 'yongki',
+                },
+                {
+                    id: '2',
+                    title: 'BraveKim',
+                },
+                {
+                    id: '3',
+                    title: 'Hello',
+                },
+            ];
+            // const result = await ClubsAPI.getClubApi();
+            fetchClubList.data = result;
+
+            return fetchClubList.data;
+        },
+    };
+
+    const [infos, setInfo] = useState(null);
+
+    useEffect(() => {
+        const getInfo = async () => {
+            const resp = await fetchClubList.fetch();
+            console.log(resp);
+            setInfo(resp);
+        };
+        getInfo();
+    }, []);
+
+    if (!infos) {
+        console.log('users is null');
+        return null;
+    }
+
     return (
-        <div>
-            <p>club list view</p>
+        <div className="container">
+            <header>
+                <div className="header-navbar">
+                    <div className="header-navbar-time">{moment(new Date()).format('h.mm')}</div>
+                    <div className="header-navbar-state">
+                        <span>
+                            <BsBarChartFill />
+                        </span>
+                        <span>
+                            <BsWifi />
+                        </span>
+                        <span>
+                            <BsBatteryFull />
+                        </span>
+                    </div>
+                </div>
+                <span className="header-modal">
+                    <BsFilterLeft />
+                </span>
+                <div className="header-content">
+                    <h1 className="header-title">삼육단지</h1>
+                    <div className="header-search">
+                        <input type="text" />
+                        <button type="button">
+                            <AiOutlineSearch />
+                        </button>
+                    </div>
+                </div>
+            </header>
+            <main>
+                <div className="main-wrapper">
+                    <div className="main-cards">
+                        {infos.map((info) => (
+                            <PostCard title={info.title} />
+                        ))}
+                    </div>
+                </div>
+            </main>
         </div>
     );
 };
