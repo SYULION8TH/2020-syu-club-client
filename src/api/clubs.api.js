3feef89d1623d560e405c79644b57372d34fdccc
Brave Kim <ygflove@likelion.org>
2020-09-01 21:28:47 +0900

    작업완료 #15

--- src/api/clubs.api.js
+++ src/api/clubs.api.js
@@ -1,11 +1,28 @@
 import axios from '../lib/axios';
 
-export async function getSomething() {
+export async function getClubApi() {
     const host = process.env.REACT_APP_API_HOST;
 
     const response = await axios({
-        url: `${host}/something`,
+        url: `${host}/club/list`,
+    });
+    return response.data;
+}
+export const getClubsSearch = async (search) => {
+    const host = process.env.REACT_APP_API_HOST;
+    const resposne = await axios({
+        url: `${host}/clubs?limit=10&search=${search}`
+    })
+
+    return resposne.data;
+}
+export async function getClubs() {
+    const host = process.env.REACT_APP_API_HOST;
+    const response = await axios({
+        url: `${host}/clubs`
     });
 
     return response.data;
 }
+
+
